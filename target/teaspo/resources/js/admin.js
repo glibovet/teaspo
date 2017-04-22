1d1af72f20bf7976565aeb086198b3fb0183ddd2
unknown <panna1997@yandex.ru>
2017-04-22 13:12:00 +0300

    delete target and idea folders

--- target/teaspo/resources/js/admin.js
+++
@@ -1,8 +0,0 @@
-function showUsers() {
-    var x = document.getElementById('allUsers');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
